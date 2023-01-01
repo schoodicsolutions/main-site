c2912fbdf71436d47ccbf2aff3c16462974c8c2f
Ryan Prescott <rppgoat@gmail.com>
2022-12-31 23:22:46 -0500

    Fix font weight

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,5 +1,5 @@
 <nav aria-label="Main" class="p-0">
-    <ul class="flex gap-8 text-white">
+    <ul class="flex gap-8 text-white font-semibold">
         <li>Home</li>
         <li>Services</li>
         <li>Contact</li>
