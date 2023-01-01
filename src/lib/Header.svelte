91940451decdeb57e58cb33e63edb82eaf125f73
Ryan Prescott <rppgoat@gmail.com>
2022-12-31 23:22:26 -0500

    Add dummy navbar

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Logo from "./Logo.svelte";
+	import Nav from "./Nav.svelte";
 
     export let variant: 'transparent' | 'solid' = 'solid';
 
@@ -14,7 +15,8 @@
     class:bg-white={solid}
     class:shadow-lg={solid}
 >
-    <div class="flex max-w-[1440px] px-3 lg:px-8 mx-auto justify-between">
+    <div class="flex max-w-[1440px] px-3 lg:px-8 mx-auto justify-between items-center">
         <Logo variant={transparent ? 'white' : solid ? 'normal' : undefined} />
+        <Nav />
     </div>
 </header>
---
+++ src/lib/Nav.svelte
@@ -0,0 +1,7 @@
+<nav aria-label="Main" class="p-0">
+    <ul class="flex gap-8 text-white">
+        <li>Home</li>
+        <li>Services</li>
+        <li>Contact</li>
+    </ul>
+</nav>
