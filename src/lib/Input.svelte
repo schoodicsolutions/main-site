a32b5383d38ba22a8b1ff18f9f95f142c9d57234
Ryan Prescott <ryan@schoodic.io>
2022-12-31 20:44:49 -0500

    Input component no styling

---
+++ src/lib/Input.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let name: string;
+    export let type: string | undefined = undefined;
+    export let label: string | undefined = undefined;
+    export let required: boolean = false;
+</script>
+
+<div>
+    {#if label}
+        <label for={name}>
+            {label}
+        </label>
+    {/if}
+    <input {type} {name} id={name} {required} />
+</div>
