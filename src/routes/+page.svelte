d99b45a2e92f1691eb9a74be585557c00fa0ec50
Ryan Prescott <rppgoat@gmail.com>
2022-12-31 22:13:59 -0500

    Do some componentizing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import Input from "$lib/Input.svelte";
-import Mark from "$lib/Mark.svelte";
+	import Hero from "$lib/Hero.svelte";
+import Input from "$lib/Input.svelte";
+    import Mark from "$lib/Mark.svelte";
 	import Wave from "$lib/Wave.svelte";
     
     let scrollY: number;
@@ -14,44 +15,16 @@ import Mark from "$lib/Mark.svelte";
 <svelte:window bind:scrollY />
 
 <main>
-    <div class="bg-croix" bind:clientHeight style:opacity>
-        <div 
-            class="lg:h-[768px] h-[calc(100vh+32px)] max-w-[1440px] mx-auto overflow-hidden relative pt-40 flex justify-center lg:justify-start"
-        >
-            <div class="absolute left-1/2 lg:transform-none -translate-x-1/2 bottom-0 lg:static w-[1024px] lg:w-[800px]">
-                <Mark width="100%" />
-            </div>
-            <div class="flex absolute top-0 h-full w-full flex-col lg:flex-row  px-4">
-                <div class="w-full" />
-                <div class="w-full flex flex-col gap-5 items-center justify-center grow text-white text-center">
-                    <h1 class="text-5xl md:text-6xl font-extrabold tracking-tight">
-                        Let's Design<br />
-                        Your Business<br />
-                        Together<br />
-                    </h1>
-                    <h2 class="text-2xl md:text-4xl leading-6 lg:leading-7 font-semibold tracking-tight">
-                        Boutique Website Design<br/>
-                        & App Development<br/>
-                        in Downeast Maine
-                    </h2>
-                </div>
-            </div>
-        </div>
-        <div class="absolute bottom-0 w-full h-8 lg:h-[6vw]">
-            <Wave class="text-white" height="100%" />
-        </div>
-    </div>
+    <Hero bind:clientHeight {opacity} />
 
-    <div class="py-6 lg:py-12">
-        <h2 class="text-5xl font-extrabold tracking-tight text-center">
+    <section class="py-6 lg:py-12">
+        <h2>
             Services We Offer
         </h2>
-    </div>
+    </section>
     
-    <div class="flex flex-col gap-4 py-6 lg:py-12 bg-almond">
-        <h2 class="text-5xl font-extrabold tracking-tight text-center">
-            Contact
-        </h2>
+    <section class="bg-almond">
+        <h2>Contact</h2>
         <form class="flex flex-col gap-4 items-center">
             <Input name="name" label="Name" placeholder="John Doe" required />
             <Input name="name" label="Name" placeholder="John Doe" required />
@@ -61,24 +34,5 @@ import Mark from "$lib/Mark.svelte";
             <Input name="name" label="Name" placeholder="John Doe" required />
             <Input name="name" label="Name" placeholder="John Doe" required />
         </form>
-    </div>
+    </section>
 </main>
-
-<footer>
-    <div class="w-full h-8 lg:h-[6vw] bg-almond">
-        <Wave class="text-croix" height="100%" />
-    </div>
-    <div class="bg-croix text-white">
-        <div class="flex justify-center gap-4 py-20">
-            <div class="flex flex-col">
-                <h2>Services</h2>
-                <p>Lorem Ipsum</p>
-                <p>Lorem Ipsum</p>
-                <p>Lorem Ipsum</p>
-                <p>Lorem Ipsum</p>
-                <p>Lorem Ipsum</p>
-            </div>
-        </div>
-    </div>
-</footer>
-
