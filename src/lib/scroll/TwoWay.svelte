ddf04924738df89cd819452696f3eb870eca7fde
Ryan Prescott <rppgoat@gmail.com>
2023-01-01 18:04:44 -0500

    Add some debouncing

--- src/lib/scroll/TwoWay.svelte
+++ src/lib/scroll/TwoWay.svelte
@@ -2,20 +2,25 @@
 	import { TOP } from "./constants";
 	import { currentSection, sections } from "./stores";
 
-    function getSectionBounds() {
-        if (window.scrollY === 0) {
-            $currentSection = TOP;
-            return;
-        }
+    let timer: number | NodeJS.Timeout;
 
-        for (let [name, section] of $sections.entries()) {
-            const boundingRect = section.getBoundingClientRect();
-            console.log(name, boundingRect.top, window.scrollY);
-            if (boundingRect.top <= window.screen.height / 2 && boundingRect.top > 0) {
-                $currentSection = name;
-                break;
+    function getSectionBounds() {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            if (window.scrollY === 0) {
+                $currentSection = TOP;
+                return;
             }
-        }
+
+            for (let [name, section] of $sections.entries()) {
+                const boundingRect = section.getBoundingClientRect();
+                console.log(name, boundingRect.top, window.scrollY);
+                if (boundingRect.top <= window.screen.height / 2 && boundingRect.top > 0) {
+                    $currentSection = name;
+                    break;
+                }
+            }
+        }, 100);
     }
 </script>
 
