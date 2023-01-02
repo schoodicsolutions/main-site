dc74065c2eb59142c3f5cdaf1f0bde7dedd2de19
Ryan Prescott <rppgoat@gmail.com>
2023-01-01 22:46:19 -0500

    Fix centering and tracking on service pages

--- src/routes/(services)/+layout.svelte
+++ src/routes/(services)/+layout.svelte
@@ -4,6 +4,6 @@
 </script>
 <slot />
 <section class="bg-almond">
-    <h3 class="font-bold text-4xl">Want to work with us?</h3>
+    <h3 class="font-bold text-4xl text-center px-6 tracking-tight">Want to work with us?</h3>
     <a class="button button-blue" use:scrollTo={{pathname: '/', scrollTo: 'contact'}}>Get in Touch</a>
 </section>
