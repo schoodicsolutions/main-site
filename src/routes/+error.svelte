e8c95d1f79d414f3f048cdf1d5c19fb29e0361c9
Ryan Prescott <rppgoat@gmail.com>
2023-01-01 21:16:03 -0500

    Fix error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,7 @@
 	import { page } from "$app/stores"
 </script>
 
-<main class="block bg-croix text-white w-screen h-screen flex flex-col gap-6 justify-center items-center">
+<main class="block bg-croix text-white w-screen h-screen flex flex-col gap-6 px-6 justify-center items-center">
     <h1>{$page.error && $page.error.message}</h1>
-    <h3 class="text-xl">Whatever you were looking for, isn't here!</h3>
+    <p class="text-xl center">Whatever you were looking for, isn't here!</p>
 </main>
