fca3abc4152731a1f665f9a2c89c571ffb8c8406
Ryan Prescott <rppgoat@gmail.com>
2023-01-02 22:11:07 -0500

    Remove pageLoad

--- src/routes/(md)/pageLoad.ts
+++
@@ -1,8 +0,0 @@
-import type { Metadata } from "src/types/metadata";
-
-export async function load() {
-    console.log(import.meta.glob('./*'));
-    const page = Object.values(import.meta.glob('./+page.md'))[0];
-    const { metadata } = await page() as {metadata: Metadata};
-    return metadata;
-}
