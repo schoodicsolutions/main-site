94bff8f9e8d65d253dd589e88e4e1411eeca2e8d
Ryan Prescott <rppgoat@gmail.com>
2023-01-04 00:13:18 -0500

    Use SMTP to send contact form entries

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,27 +1,50 @@
-import { STATICFORMS_API } from '$env/static/private'
-import { error, type Actions, type HttpError } from '@sveltejs/kit';
+import { SMTP_SERVER, SMTP_USERNAME, SMTP_PASSWORD, SMTP_FROM, SMTP_PORT, SMTP_RCPT, SMTP_SUBJECT } from '$env/static/private'
+import nodemailer from 'nodemailer';
+import { error, type Actions } from '@sveltejs/kit';
 
 export const actions: Actions = {
     default: async (event) => {
+
         const data = await event.request.formData();
 
-        data.append('_subject', 'Contact form from schoodic.io')
+        const html = Array.from(data.entries()).map(
+            ([key, value]) => {
+                if (key === 'message') {
+                    return `<br><br>${value}`;
+                } else {
+                    return `<b>${key[0].toUpperCase() + key.slice(1)}</b>: ${value}<br>`;
+                }
+            }
+        ).join('\n');
 
-        const email = data.get('email');
-        if (email) {
-            data.append('_replyTo', email);
-        }
-
-        const response = await fetch(STATICFORMS_API, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/x-www-form-urlencoded',
-            },
-            body: data,
+        const transport = nodemailer.createTransport({
+            host: SMTP_SERVER,
+            port: Number(SMTP_PORT),
+            secure: true,
+            authMethod: 'LOGIN',
+            auth: {
+                user: SMTP_USERNAME,
+                pass: SMTP_PASSWORD,
+            }
         });
 
-        if (response.status >= 400) {
-            throw error(500, 'Server error')
-        }
+        await new Promise(
+            (resolve, reject) => {
+                transport.sendMail({
+                    from: SMTP_FROM,
+                    to: SMTP_RCPT,
+                    subject: SMTP_SUBJECT,
+                    headers: {
+                        'Reply-To': SMTP_FROM,
+                    },
+                    html,
+                }, (err, info) => {
+                    if (err) reject(err);
+                    resolve(info);
+                })
+            }
+        ).catch(
+            (e) => { console.log(e); throw error(500, 'Server error') }
+        );
     }
 }
