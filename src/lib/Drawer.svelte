cba4ed047d7b7eec0c8c15b31980ac68ceb9473f
Ryan Prescott <ryan@schoodic.io>
2023-01-06 21:52:22 -0500

    Hide overflow-x

--- src/lib/Drawer.svelte
+++ src/lib/Drawer.svelte
@@ -2,7 +2,7 @@
 	import { drawerOpen } from './stores';
 </script>
 {#if $drawerOpen}
-    <div class="z-40 fixed inset-0 bg-black/50" aria-hidden={!$drawerOpen} on:click={() => $drawerOpen = !$drawerOpen} />
+    <div class="z-40 fixed inset-0 bg-black/50" aria-hidden={true} on:click={() => $drawerOpen = !$drawerOpen} />
 {/if}
 <div class="w-60 md:w-80 h-screen z-50 fixed top-0 right-0 bg-white shadow transform transition-transform p-6" class:translate-x-full={!$drawerOpen}>
     <slot />
