02199f96fecbbd45fa98fe3f6f13771826b5d71d
Ryan Prescott <rppgoat@gmail.com>
2023-02-05 22:21:43 -0500

    Add canonical url

--- src/lib/Meta.svelte
+++ src/lib/Meta.svelte
@@ -6,14 +6,23 @@
         'Schoodic Media', 
         $page.data.suffix
     ].filter(v => !!v).join(' | ');
+
+    const stripUrl = new URL($page.url);
+
+    [...stripUrl.searchParams.entries()].forEach(
+        ([key]) => stripUrl.searchParams.delete(key)
+    );
+
+    stripUrl.hash = "";
 </script>
 
 <svelte:head>
     <title>{titleString}</title>
     <meta name="description" content={$page.data.description || ''} />
-
-    <meta property="og:url" content={$page.url.toString()} />
+    <meta property="og:url" content={stripUrl.toString()} />
     <meta property="og:title" content={titleString || ''} />
     <meta property="og:description" content={$page.data.description || ''} />
     <meta property="og:image" content={$page.data.image || ''} />
+
+    <link rel="canonical" href={stripUrl.toString()} />
 </svelte:head>
