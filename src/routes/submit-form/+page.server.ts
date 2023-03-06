11400f6cd371cfac80b1d1e270618a4995b91982
Ryan Prescott <rppgoat@gmail.com>
2023-03-06 12:49:42 -0500

    Start hubspot integration; TO-DO - fix companies being added multiple times

--- src/routes/submit-form/+page.server.ts
+++ src/routes/submit-form/+page.server.ts
@@ -4,6 +4,7 @@ import { error, type Actions } from '@sveltejs/kit';
 import { createTransport }from 'nodemailer';
 import type Mail from 'nodemailer/lib/mailer';
 import type SMTPTransport from 'nodemailer/lib/smtp-transport';
+import hubspot, { AssociationTypes } from '@hubspot/api-client';
 
 import {
     SMTP_SERVER,
@@ -14,8 +15,13 @@ import {
     SMTP_RCPT,
     SMTP_SUBJECT,
     HCAPTCHA_SECRET_KEY,
-    HCAPTCHA_VERIFY_API
+    HCAPTCHA_VERIFY_API,
+    HUBSPOT_APP_TOKEN,
+    HUBSPOT_CONTACT_OWNER,
 } from '$env/static/private';
+import type { ApiException } from '@hubspot/api-client/lib/codegen/communication_preferences';
+
+const hubspotClient = new hubspot.Client({ accessToken: HUBSPOT_APP_TOKEN });
 
 const validateCaptcha = (token: string) => {
     const hcaptchaBody = new FormData();
@@ -56,6 +62,69 @@ const sendMailPromise = (mailOptions: Mail.Options) => {
     );
 }
 
+const createContactAndCompany = async ({ name, company, phone, email }: {
+    name?: string,
+    company?: string,
+    phone?: string,
+    email?: string,
+}) => {
+    if (!name || !email) return;
+
+    const nameParts = name.split(' ');
+
+    const firstname = nameParts.slice(0, -1).join(' ');
+    const lastname = nameParts.slice(-1).at(0) || '';
+
+    const contactObj: { properties: Record<string, string> }= {
+        properties: {
+            firstname,
+            lastname,
+            email,
+        },
+    }
+
+    if (phone) contactObj.properties.phone = phone;
+
+    let createContactResponse;
+    try {
+        createContactResponse = await hubspotClient.crm.contacts.basicApi.create(contactObj);
+    } catch (e) {
+        const error = e as any;
+        if (error?.code === 409) {
+            return;
+        }
+        throw e;
+    }
+
+    if (!company) return;
+
+    const forbiddenDomains = ['gmail.com', 'yahoo.com', 'hotmail.com', 'outlook.com', 'roadrunner.com', 'googlemail.com', 'rocketmail.com'];
+
+    const companyObj: { properties: Record<string, string> }= {
+        properties: {
+            name: company,
+        },
+    }
+
+    const domain = email.split('@')[1];
+    if (!forbiddenDomains.includes(domain)) companyObj.properties.domain = domain;
+
+    const createCompanyResponse = await hubspotClient.crm.companies.basicApi.create(companyObj)
+
+    await hubspotClient.crm.companies.associationsApi.create(
+        parseInt(createCompanyResponse.id),
+        'contacts',
+        parseInt(createContactResponse.id),
+        [
+            {
+                  "associationCategory": "HUBSPOT_DEFINED",
+                  "associationTypeId": AssociationTypes.companyToContact 
+                  // AssociationTypes contains the most popular HubSpot defined association types
+            }
+        ]
+    )
+}
+
 export const actions: Actions = {
     default: async (event) => {
 
@@ -84,21 +153,32 @@ export const actions: Actions = {
             }
         ).join('\n');
         
-        const fromName = data.get('name') || 'Contact Form';
+        const fromName = data.get('name')?.toString() || 'Contact Form';
         const replyToAddress = data.get('email')?.toString() || SMTP_FROM;
 
-        await sendMailPromise(
-            {
-                from: `${fromName} <${SMTP_FROM}>`,
-                to: SMTP_RCPT,
-                subject: SMTP_SUBJECT,
-                headers: {
-                    'Reply-To': `${fromName} <${replyToAddress}>`,
-                },
-                html,
+        await Promise.all([
+            sendMailPromise(
+                {
+                    from: `${fromName} <${SMTP_FROM}>`,
+                    to: SMTP_RCPT,
+                    subject: SMTP_SUBJECT,
+                    headers: {
+                        'Reply-To': `${fromName} <${replyToAddress}>`,
+                    },
+                    html,
+                }
+            ),
+            createContactAndCompany({
+                name: fromName,
+                company: data.get('company')?.toString(),
+                phone: data.get('phone')?.toString(),
+                email: data.get('email')?.toString(),
+            })
+        ]).catch(
+            (e) => { 
+                console.log(e);
+                throw error(500, 'Server Error')
             }
-        ).catch(
-            () => { throw error(500, 'Server Error') }
         )
     }
 }
