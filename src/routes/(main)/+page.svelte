b0ed411ecc61adb74e85cb9907f4811515b8e897
Ryan Prescott <rppgoat@gmail.com>
2023-08-28 23:10:07 -0400

    Add mockups and about section

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -12,30 +12,35 @@
 
     <Hero />
     <section use:scrollRef={'about'}>
-        <h2>About Us</h2>
-        <div class="w-[800px] max-w-full flex flex-col gap-4 text-lg">
-            <h4>We're a Maine Business</h4>
-            <p class="mb-8">
-               Schoodic Solutions is the only tech company in Washington County, Maine offering full stack web application development.
-               We're a family business, located in Calais, Maine and founded by cousins Ryan Prescott and Clay Coffin. Our goal is to help businesses near and far
-               make their dreams a reality.
-            </p>
-
-            <h4>Modern and Knowledgable</h4>
-            <p class="mb-8">
-               We use the latest in modern full-stack technologies to build beautiful and performant applications.
-               We're attentive to your needs, and we'll be with you 100% of the way from the initial design to the day your product goes live.
-               Schoodic Solutions is happy to work with you to make sure that your vision comes to life exactly the way you pictured it.
-            </p>
-
-            <a class="button button-blue m-auto" href="/" use:scrollTo={{section: "contact"}}>Contact Us</a>
+        <div class="flex flex-col lg:flex-row items-center lg:gap-[111px]">
+            <div class="shrink-0">
+                <img
+                    src="/assets/laptop-in-forest.webp"
+                    alt="A laptop with code on the screen, nestled among moss in the forest"
+                    class="w-full lg:w-auto lg:h-[422px] rounded-lg"
+                />
+            </div>
+            <div class="grow">
+                <h2 class="mb-9">About Schoodic Solutions</h2>
+                <h4 class="mb-3">We're a Maine Business</h4>
+                <p class="mb-7">
+                   Schoodic Solutions is the only tech company in Washington County, Maine offering full stack web application development.
+                   We're a family business, located in Calais, Maine and founded by cousins Ryan Prescott and Clay Coffin. Our goal is to help businesses near and far
+                   make their dreams a reality.
+                </p>
+    
+                <h4 class="mb-3">Modern and Knowledgable</h4>
+                <p class="mb-7">
+                   We use the latest in modern full-stack technologies to build beautiful and performant applications.
+                   We're attentive to your needs, and we'll be with you 100% of the way from the initial design to the day your product goes live.
+                   Schoodic Solutions is happy to work with you to make sure that your vision comes to life exactly the way you pictured it.
+                </p>
+            </div>
         </div>
     </section>
 
     <section use:scrollRef={'services'} class="gap-12">
-        <h2>
-            Services We Offer
-        </h2>
+        <h2 class="mb-9 text-center">Services We Offer To Our Clients</h2>
         <ServiceCards />
     </section>
     
