0021decd3dfc40f0f09d625462168cfd104b707e
Ryan Prescott <rppgoat@gmail.com>
2023-12-22 03:55:33 -0500

    fix header margin

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,56 +1,61 @@
 <script lang="ts">
 	import Logo from './Logo.svelte';
 	import Nav from './Nav.svelte';
-    import MediaQuery from 'svelte-media-queries';
+	import MediaQuery from 'svelte-media-queries';
 	import MenuIcon from './icons/MenuIcon.svelte';
 	import { drawerOpen } from './stores';
 
-    let scrollY: number = 0;
-    let scrolled = false;
+	let scrollY: number = 0;
+	let scrolled = false;
 
-    $: scrollY;
-    $: scrolled = scrollY > 100;
+	$: scrollY;
+	$: scrolled = scrollY > 100;
 
-    console.log(scrollY, scrolled);
+	console.log(scrollY, scrolled);
 </script>
 
-<svelte:window bind:scrollY={scrollY} />
+<svelte:window bind:scrollY />
 
-<header 
-    class={"sticky w-full z-30 py-2 lg:py-4 border-b border-gray-200 top-0 bg-white transition-all" + (scrolled ? ' shadow-lg' : '')}
+<header
+	class={'sticky w-full z-30 py-2 lg:py-4 border-b border-gray-200 top-0 bg-white transition-all' +
+		(scrolled ? ' shadow-lg' : '')}
 >
-    <!-- Google Tag Manager (noscript) -->
-        <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TWV5QZ3"
-                title="Google Tag Manager"
-                height="0" width="0" style="display:none;visibility:hidden">
-            </iframe>
-        </noscript>
-    <!-- End Google Tag Manager (noscript) -->
-    <div class="flex max-w-[1647px] px-5 lg:px-10 mx-auto justify-between items-center">
-        <a href="/" class="navlink">
-            <MediaQuery query="(max-width: 1024px)" let:matches>
-                <div class="ml-4 lg:m-0">
-                    {#if matches}
-                        <Logo variant="normal" size="medium"/>
-                    {:else}
-                        <Logo variant="normal" size="large"/>
-                    {/if}
-                </div>
-            </MediaQuery>
-        </a>
-        <MediaQuery query="(max-width: 1024px)" let:matches>
-            {#if matches}
-                <button 
-                    class="button icon text-black mr-1 mt-1"
-                    aria-haspopup="true"
-                    aria-label="Main Menu"
-                    on:click={() => $drawerOpen = !$drawerOpen}>
-                    <MenuIcon />
-                </button>
-            {:else}
-                <Nav />
-            {/if}
-        </MediaQuery>
-    </div>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript>
+		<iframe
+			src="https://www.googletagmanager.com/ns.html?id=GTM-TWV5QZ3"
+			title="Google Tag Manager"
+			height="0"
+			width="0"
+			style="display:none;visibility:hidden"
+		/>
+	</noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	<div class="flex max-w-screen-2xl px-5 lg:px-10 mx-auto justify-between items-center">
+		<a href="/" class="navlink">
+			<MediaQuery query="(max-width: 1024px)" let:matches>
+				<div class="ml-4 lg:m-0">
+					{#if matches}
+						<Logo variant="normal" size="medium" />
+					{:else}
+						<Logo variant="normal" size="large" />
+					{/if}
+				</div>
+			</MediaQuery>
+		</a>
+		<MediaQuery query="(max-width: 1024px)" let:matches>
+			{#if matches}
+				<button
+					class="button icon text-black mr-1 mt-1"
+					aria-haspopup="true"
+					aria-label="Main Menu"
+					on:click={() => ($drawerOpen = !$drawerOpen)}
+				>
+					<MenuIcon />
+				</button>
+			{:else}
+				<Nav />
+			{/if}
+		</MediaQuery>
+	</div>
 </header>
--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -1,30 +1,26 @@
 <script lang="ts">
 	import Mark from './Mark.svelte';
 
-    export let variant: 'normal' | 'white' = 'normal';
-    export let size: 'small' | 'medium' | 'large' = 'large';
-    $: [normal, white] = [variant === 'normal', variant === 'white']
-    $: [small, medium, large] = [size === 'small', size === 'medium', size === 'large']
+	export let variant: 'normal' | 'white' = 'normal';
+	export let size: 'small' | 'medium' | 'large' = 'large';
+	$: [normal, white] = [variant === 'normal', variant === 'white'];
+	$: [small, medium, large] = [size === 'small', size === 'medium', size === 'large'];
 </script>
 
 <div class="flex items-center select-none" aria-label="Schoodic Solutions Logo">
-    <div 
-        class:h-[35px]={large} class:h-[23px]={medium} class:h-[21px]={small} >
-        <Mark 
-            height="100%"
-            variant={normal ? 'blue' : white ? 'white' : undefined}
-        />
-    </div>
-    <span
-        class="font-brand font-bold"
-        class:text-[32px]={large}
-        class:text-[22px]={medium}
-        class:text-[19px]={small}
-        class:tracking-tight={large}
-        class:-tracking-[1.2px]={medium || small}
-        class:text-black={normal}
-        class:text-white={white}
-    >
-        Schoodic&nbsp;Solutions
-    </span>
+	<div class:h-[35px]={large} class:h-[23px]={medium} class:h-[21px]={small}>
+		<Mark height="100%" variant={normal ? 'blue' : white ? 'white' : undefined} />
+	</div>
+	<span
+		class="font-brand font-bold"
+		class:text-[32px]={large}
+		class:text-[22px]={medium}
+		class:text-[19px]={small}
+		class:tracking-tight={large}
+		class:-tracking-[1.2px]={medium || small}
+		class:text-black={normal}
+		class:text-white={white}
+	>
+		Schoodic&nbsp;Solutions
+	</span>
 </div>
